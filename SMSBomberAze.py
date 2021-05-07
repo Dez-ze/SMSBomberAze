02f36f6768ce270b19979e6aa375f751ef6cb68c
D-Deniz <83135367+D-Deniz@users.noreply.github.com>
2021-05-07 23:35:52 +0400

    Update SMSBomberAze.py

--- SMSBomberAze.py
+++ SMSBomberAze.py
@@ -78,20 +78,14 @@ print(Fore.RED)
 
 print("""
 
- .d8888b. 888b     d888 .d8888b. 888888b.   .d88888b. 888b     d888888888b.  88888888888888888b.        d88888888888888P8888888888 
-d88P  Y88b8888b   d8888d88P  Y88b888  "88b d88P" "Y88b8888b   d8888888  "88b 888       888   Y88b      d88888      d88P 888        
-Y88b.     88888b.d88888Y88b.     888  .88P 888     88888888b.d88888888  .88P 888       888    888     d88P888     d88P  888        
- "Y888b.  888Y88888P888 "Y888b.  8888888K. 888     888888Y88888P8888888888K. 8888888   888   d88P    d88P 888    d88P   8888888    
-    "Y88b.888 Y888P 888    "Y88b.888  "Y88b888     888888 Y888P 888888  "Y88b888       8888888P"    d88P  888   d88P    888        
-      "888888  Y8P  888      "888888    888888     888888  Y8P  888888    888888       888 T88b    d88P   888  d88P     888        
-Y88b  d88P888   "   888Y88b  d88P888   d88PY88b. .d88P888   "   888888   d88P888       888  T88b  d8888888888 d88P      888        
- "Y8888P" 888       888 "Y8888P" 8888888P"  "Y88888P" 888       8888888888P" 8888888888888   T88bd88P     888d88888888888888888888 
+
+
                                                                                                                                                
-  with :) Deniz                            
 
-  https://t.me/skull_team_aze                                  
+                                                                                                                                                                       with :) Deniz                            
 
-  Qeyd : Biz Heç bir şeydən məsuliyyət daşımırıq.
+                                                                                                                                                                                                   
+    Qeyd : Biz Heç bir şeydən məsuliyyət daşımırıq.
 
 """)
 
@@ -105,7 +99,7 @@ def main():
 
   
 
-  target = input("[*] Nömrəni (+994) Olmadan Yaz! (0 Yazmırıq) (Məs:551234567): ")
+  target = input("[*] Nömrəni (+994) Olmadan Yaz! (0 Yazmırıq Məs:551234567): ")
 
   amount = int(input("[*] Göndəriləcək SMS sayı: "))
 
